9651426630351bb2032c5cfa98734700d7deb38a
0x6368656174 <0x6368656174@gmail.com>
2017-06-08 14:20:50 +1000

    сделан pandoc

---
+++ make.sh
@@ -0,0 +1,14 @@
+rm -r dist
+rm -r build
+mkdir -p dist
+mkdir -p build
+find . -iname "*.md" -exec cp {} build \;
+find . -iname "*.png" -exec cp {} build \;
+find . -iname "*.svg" -exec cp {} build \;
+cp title.txt build
+cp styles/epub.css build
+cd build
+
+FILES=`find . -iname "*.md" | sort`
+
+pandoc -f markdown+header_attributes -t epub3 -S --normalize --epub-chapter-level=3 --epub-stylesheet=epub.css -o ../dist/book.epub title.txt $FILES
