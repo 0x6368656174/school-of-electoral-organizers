476a67ddeb1364cee7ade19de2c81b44677ce335
0x6368656174 <0x6368656174@gmail.com>
2017-06-08 15:01:04 +1000

    deploy

--- build.sh
+++ build.sh
@@ -6,11 +6,8 @@ find . -iname "*.md" -exec cp {} build \;
 find . -iname "*.png" -exec cp {} build \;
 find . -iname "*.svg" -exec cp {} build \;
 cp title.txt build
-cp styles/epub.css build
 cd build
 
 FILES=`find . -iname "*.md" | sort`
 
-pandoc -f markdown+header_attributes -t epub3 -S --normalize --epub-chapter-level=3 --epub-stylesheet=epub.css -o ../dist/book.epub title.txt $FILES
-
-ls $TRAVIS_BUILD_DIR
+pandoc -f markdown+header_attributes -t epub3 -S --normalize --epub-chapter-level=3 -o ../dist/book.epub title.txt $FILES
