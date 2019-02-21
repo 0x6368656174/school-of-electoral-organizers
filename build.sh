9b57bf4e861a32b77e29b2ab978fa9fcdca675a1
Pavel Puchkov <0x6368656174@gmail.com>
2019-02-21 15:21:30 +1000

    build: fix

--- build.sh
+++ build.sh
@@ -60,4 +60,4 @@ done
 
 FILES=`find . -iname "*.md" | sort`
 
-pandoc -f markdown+header_attributes -t epub3 -S --normalize --epub-chapter-level=4 --toc-dept=4 -o ../dist/book-svg.epub title.txt $FILES
+pandoc -f markdown+header_attributes+smart -t epub3 --epub-chapter-level=4 --toc-dept=4 -o ../dist/book-svg.epub title.txt $FILES
